a3ff6d12078d3f2ed7c97580a03f8f24cfdc68f9
joaopedrohagen <joaopedro@hagen.dev.br>
2025-07-02 16:15:24 -0300

    feat: buttons to delete pods

--- app/bot/__init__.py
+++ app/bot/__init__.py
@@ -1,3 +1,3 @@
-from .handlers import pod_status
+from .handlers import pod_status, callback_button
 
-__all__ = ["pod_status"]
+__all__ = ["pod_status", "callback_button"]
--- app/bot/handlers.py
+++ app/bot/handlers.py
@@ -1,10 +1,29 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
-from app.services.kubernetes import get_pods
+from app.services.kubernetes import get_pods, delete_pods
 from app.utils.logger import logger
 
 class CustomBotError(Exception):
     pass
+
+async def callback_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+
+    if not query or not query.data:
+        raise CustomBotError("Sem Callback")
+
+    await query.answer()
+
+    if query.data.startswith("restart"):
+        try:
+            _, namespace, pod_name = query.data.split("|")
+            delete_pods(namespace=namespace, label=pod_name)
+            await query.edit_message_text(f"Pod {pod_name} reiniciado!", parse_mode="Markdown")
+
+        except Exception as e:
+            logger.error(f"Erro ao deletar recurso!")
+            await query.edit_message_text(f"Erro ao deletar pod!")
+
 
 async def pod_status(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.effective_user:
@@ -26,9 +45,13 @@ async def pod_status(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         pods = get_pods(namespace)
         for pod in pods:
-            await update.message.reply_text(
-                f"POD: {pod.name}\nNamespace: {pod.ns}\nStatus: {pod.status}"
-            )
+            keyboard = [
+                [InlineKeyboardButton("Restart", callback_data=f"restart|{pod.ns}|{pod.name}")]
+            ]
+
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            await update.message.reply_text(f"POD: {pod.name}\nNamespace: {pod.ns}\nStatus: {pod.status}", reply_markup=reply_markup)
+
     except Exception as e:
         logger.error(f"Erro ao buscar pods: {e}")
         await update.message.reply_text("Erro ao buscar os pods.")
