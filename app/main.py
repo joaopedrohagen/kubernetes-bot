6b667ae3f4e00ba47e256adde1a404f7f540e989
joaopedrohagen <joaopedro@hagen.dev.br>
2025-07-10 10:07:05 -0300

    feat: error handling and function to get the configmaps

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
+from typing import Any
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 from app.bot.commands import UserCommands
 from app.bot.callback_router import Callbacks
 from app.config.settings import settings
-from app.services.kubernetes import KubernetesClient
+from telegram import BotCommand
+
+async def set_bot_commands(app: Any):
+    await app.bot.set_my_commands(
+        [
+            BotCommand("pod_status", "Lista os pods no namespace passado como argumento."),
+            BotCommand("list_envs", "Lista as secrets e os configmaps no namespace passado como argumento.")
+        ]
+    )
 
 def main():
-    app = ApplicationBuilder().token(settings["telegram_bot_token"]).build()
+    app = ApplicationBuilder().token(settings["telegram_bot_token"]).post_init(set_bot_commands).build()
 
     app.add_handler(CommandHandler("pod_status", lambda update, context: UserCommands(update, context).pod_status()))
     app.add_handler(CommandHandler("list_envs", lambda update, context: UserCommands(update, context).list_envs()))
--- app/services/kubernetes.py
+++ app/services/kubernetes.py
@@ -1,3 +1,5 @@
+import re
+from typing import Dict
 from typing import NamedTuple, Any, Optional
 from kubernetes import client, config
 from kubernetes.client import ApiException
@@ -22,20 +24,25 @@ class KubernetesClient:
     self.v1 = client.CoreV1Api()
 
   def get_pods(self, namespace: str, label: Optional[str] = None) -> Any:
-    pods = self.v1.list_namespaced_pod(namespace=namespace, label_selector=label, watch=False)
+    try:
+      pods = self.v1.list_namespaced_pod(namespace=namespace, label_selector=label, watch=False)
 
-    pod_list = [
-        KubeResourceInfo(
-            name=pod.metadata.name,
-            ns=pod.metadata.namespace,
-            status=pod.status.phase
-        ) for pod in pods.items
-    ]
+      pod_list = [
+          KubeResourceInfo(
+              name=pod.metadata.name,
+              ns=pod.metadata.namespace,
+              status=pod.status.phase
+          ) for pod in pods.items
+      ]
 
-    logger.info(f"{len(pod_list)} pods encontrados em {namespace}")
-    return pod_list
+      logger.info(f"{len(pod_list)} pods encontrados em {namespace}")
+      return pod_list
+
+    except ApiException as e:
+      logger.error(f"Erro ao recuperar pods! {e.reason}: {e.body}")
 
   def get_secrets(self, namespace: str) -> Any:
+    try:
       secrets = self.v1.list_namespaced_secret(namespace=namespace)
 
       secrets_list = [
@@ -48,7 +55,12 @@ class KubernetesClient:
       logger.info(f"{len(secrets_list)} secrets encontradas em {namespace}")
       return secrets_list
 
+    except ApiException as e:
+      logger.error(f"Erro ao recuperar secrets! {e.reason}: {e.body}")
+      return None
+
   def get_secrets_data(self, name: str, namespace: str) -> Any:
+    try:
       secret: Any = self.v1.read_namespaced_secret(name=name, namespace=namespace)
       data = secret.data or {}
 
@@ -59,12 +71,15 @@ class KubernetesClient:
 
       return decoded
 
+    except ApiException as e:
+      logger.error(f"Erro ao recuperar dados da secret! {e.reason}: {e.body}")
+
   def delete_pods(self, namespace: str, pod_name: str):
       try:
           self.v1.delete_namespaced_pod(namespace=namespace, name=pod_name)
           logger.info(f"Deletando pod {pod_name}.")
       except ApiException as e:
-          logger.error(f"Falha ao deletar {namespace}/{pod_name}: {e.reason}")
+          logger.error(f"Falha ao deletar {namespace}/{pod_name}! {e.reason}: {e.body}")
 
   def pod_logs(self, namespace: str, pod_name: str) -> Any:
       try:
@@ -74,6 +89,39 @@ class KubernetesClient:
           return logs
 
       except ApiException as e:
-          logger.error(f"Falha ao recuperar log do {pod_name}: {e.body}")
+          logger.error(f"Falha ao recuperar log do {pod_name}! {e.reason}: {e.body}")
 
           return None
+
+  def get_config_map(self, namespace: str) -> Any:
+    try:
+      config_maps = self.v1.list_namespaced_config_map(namespace=namespace)
+
+      config_maps_list = [
+        KubeResourceInfo(
+            name=cm.metadata.name,
+            ns=cm.metadata.namespace
+        ) for cm in config_maps.items
+      ]
+
+      logger.info(f"{len(config_maps_list)} encontrados em {namespace}")
+      return config_maps_list
+
+    except ApiException as e:
+      logger.error(f"Erro ao recuperar ConfigMaps! {e.reason}: {e.body}")
+
+  def get_config_map_data(self, name: str, namespace: str) -> Optional[Dict[str, str]]:
+    try:
+      config_map: Any = self.v1.read_namespaced_config_map(name=name, namespace=namespace)
+      data = config_map.data or {}
+
+      data_formatted = {
+        key: value
+        for key, value in data.items()
+      }
+
+      return data_formatted
+
+    except ApiException as e:
+      logger.error(f"Erro ao recuperar dados do ConfigMap! {e.reason}: {e.body}")
+      return None
