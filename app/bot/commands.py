436decaee8b61e3f0b19acc6f7860506e470c806
joaopedrohagen <joaopedro@hagen.dev.br>
2025-07-10 16:57:57 -0300

    refactor: decrease in repetitions

--- app/bot/commands.py
+++ app/bot/commands.py
@@ -1,6 +1,9 @@
+from types import LambdaType
+from typing import Dict
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from app.services.kube_client import kube_client as kube
+from app.services.kubernetes import KubeResourceInfo
 from app.utils.logger import logger
 from app.bot.exceptions import MissingMessage, MissingArgs
 
@@ -10,59 +13,74 @@ class UserCommands:
     self.context = context
     self.args = context.args
 
+  async def _verify_namespace(self):
+    if not self.update.message:
+      raise MissingMessage("Mensagem não encontrada!")
+
+    if not self.args:
+      await self.update.message.reply_text("🟡 Você não passou nenhum namespace.")
+      raise MissingArgs ("Argumento não encontrado!")
+
+    return self.args[0]
+
+  async def _list_resources(self, resource: str, resource_list: list[KubeResourceInfo], buttons: Dict[str, str], func_message: LambdaType):
+    if not self.update.message:
+      raise MissingMessage("Mensagem não encontrada!")
+
+    if not self.args:
+      await self.update.message.reply_text("🟡 Você não passou nenhum namespace.")
+      raise MissingArgs ("Argumento não encontrado!")
+
+    namespace = await self._verify_namespace()
+    await self.update.message.reply_text(f"Listando {resource} no namespace {namespace}...")
+
+    try:
+      for r in resource_list:
+        keyboard = [[
+          InlineKeyboardButton(f"{value}", callback_data=f"{key}|{r.ns}|{r.name}")
+          for key, value in buttons.items()
+        ]]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await self.update.message.reply_text(f"{func_message(r)}", reply_markup=reply_markup)
+
+    except Exception as e:
+      logger.error(f"Erro ao buscar pods: {e}")
+      await self.update.message.reply_text(f"🔴 Erro ao buscar {resource}.")
+
+
   async def pod_status(self):
-    if not self.update.message:
-      raise MissingMessage("Mensagem não encontrada!")
+    namespace = await self._verify_namespace()
+    pods = kube.get_pods(namespace)
+    resource = "pods"
+    func_message = lambda pod: f"POD: {pod.name}\nNamespace: {pod.ns}\nStatus: {pod.status}"
+    buttons = {
+      "restart": "Restart 🔁",
+      "logs": "Logs 📄"
+    }
 
-    if not self.args:
-      await self.update.message.reply_text("🟡 Você não passou nenhum namespace.")
-      raise MissingArgs ("Argumento não encontrado!")
+    await self._list_resources(resource=resource, resource_list=pods, func_message=func_message, buttons=buttons)
 
-    namespace = self.args[0]
-    await self.update.message.reply_text(f"Listando pods no namespace {namespace}...")
 
-    try:
-      pods = kube.get_pods(namespace)
+  async def list_secrets(self):
+    namespace = await self._verify_namespace()
+    secrets = kube.get_secrets(namespace)
+    resource = "secrets"
+    func_message = lambda secret: f"Secret: {secret.name}\nNamespace: {secret.ns}"
+    buttons = {
+      "secrets": "Secrets 🔐"
+    }
 
-      for pod in pods:
-        keyboard = [
-          [
-            InlineKeyboardButton("Restart 🔁", callback_data=f"restart|{pod.ns}|{pod.name}"),
-            InlineKeyboardButton("Logs 📄", callback_data=f"logs|{pod.ns}|{pod.name}")
-          ]
-        ]
+    await self._list_resources(resource=resource, resource_list=secrets, func_message=func_message, buttons=buttons)
 
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await self.update.message.reply_text(f"POD: {pod.name}\nNamespace: {pod.ns}\nStatus: {pod.status}", reply_markup=reply_markup)
+  async def list_config_maps(self):
+    namespace = await self._verify_namespace()
+    configmap = kube.get_config_map(namespace)
+    resource = "configmaps"
+    func_message = lambda cm: f"ConfigMap: {cm.name}\nNamespace: {cm.ns}"
+    buttons = {
+      "configmaps": "ConfigMap 🔐"
+    }
 
-    except Exception as e:
-        logger.error(f"Erro ao buscar pods: {e}")
-        await self.update.message.reply_text("🔴 Erro ao buscar os pods.")
-
-  async def list_envs(self):
-    if not self.update.message:
-      raise MissingMessage("Mensagem não encontrada!")
-
-    if not self.args:
-      await self.update.message.reply_text("🟡 Você não passou nenhum namespace.")
-      raise MissingArgs ("Argumento não encontrado!")
-
-    namespace = self.args[0]
-    await self.update.message.reply_text(f"Listando secrets no namespace {namespace}")
-
-    try:
-      secrets = kube.get_secrets(namespace)
-      for secret in secrets:
-        keyboard = [
-          [
-            InlineKeyboardButton("Secrets 🔐", callback_data=f"secrets|{secret.ns}|{secret.name}")
-          ]
-        ]
-
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await self.update.message.reply_text(f"Secret: {secret.name}\nNamespace: {secret.ns}", reply_markup=reply_markup)
-
-    except Exception as e:
-      logger.error(f"Erro ao buscar secret: {e}")
-      await self.update.message.reply_text("🔴 Erro ao buscar as secrets.")
+    await self._list_resources(resource=resource, resource_list=configmap, func_message=func_message, buttons=buttons)
 
